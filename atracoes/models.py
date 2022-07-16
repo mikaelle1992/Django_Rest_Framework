4dbcb62f2dea05b9c87b8f8c6540ebf9f33f96d0
Mikaelle Rubia Pinheiro Sousa <mrubia@apoioprodesp.sp.gov.br>
2022-07-16 17:52:52 -0300

    aplicações migradas

---
+++ atracoes/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Atracao(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField()
+    horario_func = models.TextField()
+    idade_minina = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
