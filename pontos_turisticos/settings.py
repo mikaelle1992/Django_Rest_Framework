63a5f7c2324a5f3baf5d52fb88fa7ba97a7bfc84
Mikaelle <mikaelle.rubiaps02@gmail.com>
2023-01-16 20:40:18 -0300

    feature: upload de imagens

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -127,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT = 'imagens'
+
+MEDIA_URL = '/media/'
